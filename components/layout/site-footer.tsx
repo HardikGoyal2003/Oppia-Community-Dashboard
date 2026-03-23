21b39d70ca26cbbff5515e5f36ef9cdc46e98030
hardikgoyal2003 <hardikgoyal2003@gmail.com>
2026-03-23 18:15:25 +0530

    Added License, and updated readme file

--- components/layout/site-footer.tsx
+++ components/layout/site-footer.tsx
@@ -2,6 +2,9 @@ type SiteFooterProps = {
   className?: string;
 };
 
+const REPO_URL = "https://github.com/HardikGoyal2003/Oppia-Leads-Dashboard";
+const ISSUES_URL = `${REPO_URL}/issues`;
+
 export function SiteFooter({ className = "" }: SiteFooterProps) {
   return (
     <footer className={className}>
@@ -14,6 +17,14 @@ export function SiteFooter({ className = "" }: SiteFooterProps) {
           className="underline-offset-4 hover:underline"
         >
           GitHub
+        </a>
+        <a
+          href={ISSUES_URL}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline-offset-4 hover:underline"
+        >
+          Report an issue
         </a>
         <a
           href="https://www.linkedin.com/in/hardikgoyal2003"
