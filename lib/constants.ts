860ec5770bfc2d76a377e9f2d83648008ce25a70
hardikgoyal2003 <hardikgoyal2003@gmail.com>
2026-03-25 15:59:16 +0530

    Disabled the announcement banner

--- lib/constants.ts
+++ lib/constants.ts
@@ -28,7 +28,7 @@ export const CONSTANTS = {
     TITLE: "🎉 New version is live:",
     MESSAGE:
       "Explore the latest Oppia Community Dashboard and feel free to share your feedback or any bug reports.",
-    IS_ENABLED: true,
+    IS_ENABLED: false,
   },
 
   ROLES: {
