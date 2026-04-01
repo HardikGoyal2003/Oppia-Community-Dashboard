02482fe490d14896a9a2a959682fd0e86021dde3
hardikgoyal2003 <hardikgoyal2003@gmail.com>
2026-04-01 23:52:09 +0530

    Enabled sidebar for team lead view

--- features/dashboard/views/team-lead-view/team-lead.view.tsx
+++ features/dashboard/views/team-lead-view/team-lead.view.tsx
@@ -1,7 +1,37 @@
 "use client";
 
+import { Bug } from "lucide-react";
+import { AppSidebar } from "@/components/layout/sidebar/app-sidebar";
+import { Navbar } from "@/components/layout/navbar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import UnansweredIssuesTab from "../../shared/unanswered-issues.tab";
 
 export default function TeamLeadView() {
-  return <UnansweredIssuesTab />;
+  const sidebarItems = [
+    {
+      name: "Unanswered Issue",
+      icon: Bug,
+    },
+  ];
+
+  return (
+    <SidebarProvider>
+      <AppSidebar
+        items={sidebarItems}
+        activeItemName="Unanswered Issue"
+        onItemSelect={() => {}}
+      />
+      <SidebarInset>
+        <Navbar leftContent={<SidebarTrigger className="-ml-1" />} />
+
+        <div className="min-h-screen bg-gray-50 px-6 py-10">
+          <UnansweredIssuesTab />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
